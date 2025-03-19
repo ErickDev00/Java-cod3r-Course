47803cba414a535bc146239fb95bf09b6a7a8e80
Erick <profissional.erick@gmail.com>
2025-03-19 11:43:58 -0300

    notacaoPonto

---
+++ notacaoPonto/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 2.3;
+        String s = "Bom dia X!";
+
+        s = s.replace("X", "SENHORA");
+        s = s.toUpperCase();
+
+        System.out.println(s);
+    }
+}
