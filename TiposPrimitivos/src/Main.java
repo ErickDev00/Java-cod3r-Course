26018534b21598027e636fe28019a9fb73fc9c2c
Erick <profissional.erick@gmail.com>
2025-03-19 10:40:54 -0300

    tiposAndInterferencia

---
+++ TiposPrimitivos/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        //tipos numericos inteiros
+        byte anosDeEmpresa = 23;
+        short numeroDeVoos = 542;
+        int id = 56789;
+        long pontosAcumulados = 3_234_845_223L;
+
+        //tipos numericos reais
+        float salario = 3000;
+        double vendasAcumuladas = 2_991_797_103.07;
+    }
+}
