874d0f46101c3eb34704f2a1c49b6bc89f964eb5
Erick <profissional.erick@gmail.com>
2025-03-20 12:23:29 -0300

    stringEAtributos

---
+++ String/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!".charAt(2));
+
+        String s = "Erick";
+        s = s.toUpperCase();
+        System.out.println(s.concat("!!!"));
+    }
+}
