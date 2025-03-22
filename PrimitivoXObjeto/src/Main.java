ae0cfe806a58eaa9c13a695d77a09d3512030f37
Erick <profissional.erick@gmail.com>
2025-03-22 13:11:23 -0300

    wrappers

--- PrimitivoXObjeto/src/Main.java
+++ PrimitivoXObjeto/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String s = "texto"; //isso é um objeto.
@@ -5,5 +7,20 @@ public class Main {
         int a = 123; //isso é um tipo primitivo (não tem notação ponto e só carrega o valor. ex: a.)
 
         //Wrappers (versão objeto dos tipos primitivos)
+
+        Scanner entrada = new Scanner(System.in);
+
+        Byte b = 100;
+        Short c = 1000;
+
+
+        Integer i = Integer.parseInt(entrada.next());
+        Long l = 1000000L;
+
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i*3);
+
+        entrada.close();
     }
 }
