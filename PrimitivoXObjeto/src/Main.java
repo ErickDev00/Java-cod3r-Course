ede31a72c2a6a42d5822392edd0adadaf9b6e37c
Erick <profissional.erick@gmail.com>
2025-03-22 13:04:40 -0300

    primitivoeobjeto

---
+++ PrimitivoXObjeto/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "texto"; //isso é um objeto.
+
+        int a = 123; //isso é um tipo primitivo (não tem notação ponto e só carrega o valor. ex: a.)
+
+        //Wrappers (versão objeto dos tipos primitivos)
+    }
+}
