bb32f1f7a3b3066e686d08afeda6f804b8b6ab9d
Erick <profissional.erick@gmail.com>
2025-03-24 12:06:59 -0300

    converterTipos

---
+++ ConversaoTipoPrimitivo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 1;// implicita
+
+        float b = 1.0F;
+        float ba = (float) 1.12343; //conversao explicita (ira perder casas decimais) CAST
+    }
+}
