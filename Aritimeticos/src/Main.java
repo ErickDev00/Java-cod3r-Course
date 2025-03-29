cccae07b7396a5c6e3811209e906c2dfe2afd18c
Erick <profissional.erick@gmail.com>
2025-03-29 12:17:22 -0300

    Operadores2

---
+++ Aritimeticos/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(2+3);
+
+        double x = 34.56;
+        var y = 2.2;
+
+        System.out.println(x+y);
+
+        int a = 8;
+        int b = 9;
+
+        System.out.println(a + b);
+        System.out.println(a / (double) b);
+    }
+}
