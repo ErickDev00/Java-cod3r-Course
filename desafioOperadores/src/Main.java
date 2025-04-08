5190c052371ea2ec02390dadb36ab9a8a6cd0d6d
Erick <profissional.erick@gmail.com>
2025-04-08 11:51:30 -0300

    desafioOperadores

---
+++ desafioOperadores/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //trabalho na terça (v ou f)
+        //trabalho na quinta (v ou f)
+
+        boolean trabalho1 = true;
+        boolean trabalho2 = false;
+
+        boolean comprouTV50 = trabalho1 & trabalho2;
+        boolean comprouTV32 = trabalho1 ^ trabalho2;
+        boolean comprouSorvete = trabalho1 || trabalho2;
+
+    }
+}
