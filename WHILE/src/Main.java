457a5b25d452ed242f97c872ff3d9e8c20c92b2b
Erick <profissional.erick@gmail.com>
2025-04-24 13:43:45 -0300

    whileAula

---
+++ WHILE/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int contador = 1;
+        while(contador <= 10){
+            System.out.println("Bom dia");
+            contador++;
+        }
+    }
+}
