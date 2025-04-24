982c808d9c06c9664edace71f2e2203fa1350234
Erick <profissional.erick@gmail.com>
2025-04-24 13:49:34 -0300

    whileAula1

---
+++ WHILE/src/For1.java
@@ -0,0 +1,24 @@
+public class For1 {
+    public static void main(String[] args) {
+
+
+        for(int cont = 1; cont <=10; cont ++){
+            //declaração da variavel, se vai continuar, por ultimo manipulação.
+            System.out.println("Boa Tarde!");
+        }
+
+        //outra maneira
+        int x = 2;
+        for(; x<10;){
+            System.out.println("x = " + x);
+            x++;
+        }
+
+        //laço infinito
+        //for(;;){
+            //System.out.println("teste");
+        //}
+
+
+    }
+}
