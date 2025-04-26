1b471a7c37e95ee5ade00e4dddc18f4bcdc5bcbc
Erick <profissional.erick@gmail.com>
2025-04-26 13:17:44 -0300

    desafioForr

---
+++ WHILE/src/DesafioFor.java
@@ -0,0 +1,17 @@
+public class DesafioFor {
+
+    public static void main(String[] args) {
+
+        String valor = "#";
+        for (int i=1; i<=5; i++){
+            System.out.println(valor);
+            valor += "#";
+        }
+
+        //fazer o for sem usar controle numerico;
+
+        for (String v="#"; !v.equals("######"); v += "#"){
+            System.out.println(v);
+        }
+    }
+}
