49c05df24e4c4639de399e8218abed6e4354df9e
Erick <profissional.erick@gmail.com>
2025-05-06 09:05:12 -0300

    ClasseEMetodos

---
+++ ClasseEMetodo/src/ProdutoTeste.java
@@ -0,0 +1,14 @@
+public class ProdutoTeste {
+
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto();
+        p1.nome = "Celular";
+        p1.preco = 3299.90;
+        p1.desconto = 0.25;
+
+
+
+
+    }
+}
