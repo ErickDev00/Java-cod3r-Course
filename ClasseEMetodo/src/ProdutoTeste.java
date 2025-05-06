0b7b9a8fb3caa61499bb6bade951533293f6e675
Erick <profissional.erick@gmail.com>
2025-05-06 09:08:45 -0300

    finalizandoAula

--- ClasseEMetodo/src/ProdutoTeste.java
+++ ClasseEMetodo/src/ProdutoTeste.java
@@ -7,6 +7,11 @@ public class ProdutoTeste {
         p1.preco = 3299.90;
         p1.desconto = 0.25;
 
+        System.out.println(p1.nome);
+
+        double precoFinal1 = p1.preco * (1 - p1.desconto);
+        System.out.println(precoFinal1);
+
 
 
 
