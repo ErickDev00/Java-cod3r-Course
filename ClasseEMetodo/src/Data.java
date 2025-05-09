95ba415d62c09b70605462881986425c2ec168a5
Erick <profissional.erick@gmail.com>
2025-05-09 14:22:27 -0300

    desafioData

---
+++ ClasseEMetodo/src/Data.java
@@ -0,0 +1,5 @@
+public class Data {
+    Integer dia;
+    String mes;
+    Integer ano;
+}
---
+++ ClasseEMetodo/src/DataTeste.java
@@ -0,0 +1,9 @@
+public class DataTeste {
+    public static void main(String[] args) {
+
+        Data d1 = new Data();
+        d1.dia = 12;
+        d1.mes = "fevereiro";
+        d1.ano = 2002;
+    }
+}
