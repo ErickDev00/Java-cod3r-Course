75185619c9e01ea524723bd3ea52567e2e5afb9f
Erick <profissional.erick@gmail.com>
2025-05-13 13:56:49 -0300

    criandoMetodo

--- ClasseEMetodo/src/ProdutoTeste.java
+++ ClasseEMetodo/src/ProdutoTeste.java
@@ -7,9 +7,15 @@ public class ProdutoTeste {
         p1.preco = 3299.90;
         p1.desconto = 0.25;
 
+        Produto p2 = new Produto();
+        p2.nome = "Tablet";
+        p2.preco = 2990.90;
+        p2.desconto = 0.35;
+
         System.out.println(p1.nome);
 
         double precoFinal1 = p1.preco * (1 - p1.desconto);
+        double precoFinal2 = p2.precoComDesconto();
         System.out.println(precoFinal1);
 
 
