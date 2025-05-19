a28eb4829069448288f595bf5a721c351e3ff180
Erick <profissional.erick@gmail.com>
2025-05-19 11:06:51 -0300

    ResultadoDesafio

---
+++ ClasseEMetodo/src/DesafioTrauma.java
@@ -0,0 +1,14 @@
+public class DesafioTrauma {
+
+    int a = 3;
+    static int b = 4;
+
+    public static void main(String[] args) {
+
+        DesafioTrauma p = new DesafioTrauma();
+        System.out.println(p.a);
+
+        System.out.println(b);
+
+    }
+}
