82c5248cca80d6a7f0c7b84fc6197607329ac39b
Erick <profissional.erick@gmail.com>
2025-06-06 13:27:22 -0300

    heranca#2

--- poo.cmposicao/src/Heranca/Heroi.java
+++ poo.cmposicao/src/Heranca/Heroi.java
@@ -1,8 +1,25 @@
 package Heranca;
 
-public class Heroi extends Jogador{ //vai receber código a partir do mecanismo de herança.
+public class Heroi extends Jogador { //vai receber código a partir do mecanismo de herança.
     //o Heroi e um jogador.
 
+
+    boolean atacar(Jogador oponente) {
+        int deltaX = Math.abs(x - oponente.x);
+        int deltaY = Math.abs(y - oponente.y);
+
+        if (deltaX == 0 && deltaY == 1) {
+            oponente.vida -= 20;
+        } else if (deltaX == 1 && deltaY == 0) {
+            oponente.vida -= 20;
+            return true;
+        } else {
+            return false;
+        }
+
+
+        return true;
+    }
 
 
 }
