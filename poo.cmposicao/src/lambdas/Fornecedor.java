5b80e8bb127e973a677732e6e71d1952499d1e32
Erick <profissional.erick@gmail.com>
2025-06-25 13:47:37 -0300

    supplier

--- poo.cmposicao/src/lambdas/Fornecedor.java
+++ poo.cmposicao/src/lambdas/Fornecedor.java
@@ -1,4 +1,26 @@
 package lambdas;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Fornecedor {
+
+    public static void main(String[] args) {
+
+        //interface funcional que não recebe nenhum parâmetro
+        Supplier<List<String>> umaLista = () -> Arrays.asList("Ana", "Teste");
+
+        System.out.println(umaLista.get());
+
+
+
+
+
+
+
+
+
+    }
+
 }
