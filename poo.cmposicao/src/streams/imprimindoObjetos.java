808e9dd2ac36386afa10ae4dd6679ca46801248b
Erick <profissional.erick@gmail.com>
2025-06-30 11:36:45 -0300

    strem aula 1

---
+++ poo.cmposicao/src/streams/imprimindoObjetos.java
@@ -0,0 +1,35 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class imprimindoObjetos {
+
+    public static void main(String[] args) {
+
+
+        List<String> aprovados = Arrays.asList("Lu", "Gui", "Ana");
+
+        /*for (int i = 0; i < aprovados.size(); i++){
+            System.out.println(aprovados.get(i));
+        } */
+
+        for (String nome: aprovados){
+            System.out.println(nome);
+        }
+
+
+        Iterator<String> it = aprovados.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+
+        Stream<String> st = aprovados.stream();
+        st.forEach(System.out::println);
+
+    }
+
+}
